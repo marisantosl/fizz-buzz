701a9f54d041c17584f6a953e90e7e514705e29b
Mari Paz <maripaz17santos@gmail.com>
2025-11-19 11:05:24 -0600

    feat: se terminó el juego fizz buzz

---
+++ script.js
@@ -0,0 +1,19 @@
+let limit = 21;
+let counter = 1;
+
+while (counter < limit) {
+    if (counter % 3 === 0 && counter % 5 === 0) 
+        {
+        console.log("FizzBuzz"); 
+      }
+    else if (counter % 3 === 0)
+        {
+        console.log("Fizz"); 
+        }  
+    else if (counter % 5 === 0) 
+        {
+        console.log("Buzz"); 
+    }
+        counter++;
+
+    }
